7cbc898aa91e0e156d497d71f1a2e24c5fc1ec84
Lord <mrfloor772@gmail.com>
2023-04-12 14:09:28 +0300

    хуйхуй

---
+++ seminar5/Program.cs
@@ -0,0 +1,169 @@
+﻿// Задача #1. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+/*
+int[] CreateArr(int size)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++) 
+        array[i] = new Random().Next(100, 1000);
+    
+    return array;
+}
+
+int PositivNum(int[] array)
+{   
+    int numbers = 0;
+    for (int j = 0; j < array.Length; j++)
+        if (array[j] % 2 == 0)
+            numbers++;
+    return numbers;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.Write("Укажите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] showArr = CreateArr(size);
+ShowArray(showArr);
+Console.WriteLine(PositivNum(showArr));
+*/
+
+// Задача #2. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+/*
+double[] CreateArr(int size, int min, int max)
+{
+    double[] array = new double[size];
+
+    for(int i = 0; i < size; i++) 
+        array[i] = Math.Round(new Random().NextDouble() + new Random().Next(min, max), 2);
+    
+    return array;
+}
+
+double SumNum(double[] array)
+{
+    double total = 0;
+    for (int j = 1; j < array.Length; j += 2) 
+        total += array[j];
+    
+    return Math.Round(total, 2);
+}
+
+void ShowArray(double[] array)
+{
+    for(int l = 0; l < array.Length; l++)
+        Console.Write(array[l] + " ");
+    Console.WriteLine();
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[] showArr = CreateArr(size, min, max);
+ShowArray(showArr);
+
+Console.WriteLine(SumNum(showArr));
+*/
+
+// Задача #3.  Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] CreateArr(int size, int min, int max)
+{
+    double[] array = new double[size];
+
+    for(int i = 0; i < size; i++) array[i] = Math.Round(new Random().NextDouble() + new Random().Next(min, max), 2);
+    
+    return array;
+}
+
+double searchNum(double[] array)
+{
+
+    double min_num = array[0];
+    double max_num = array[0];
+
+    for(int j = 0; j < array.Length; j++)
+    {
+        if (min_num > array[j])
+            min_num = array[j];
+        else if (max_num < array[j])
+            max_num = array[j];
+    }
+    double total = max_num - min_num;
+
+    return Math.Round(total, 2);
+}
+
+void ShowArray(double[] array)
+{
+    for(int k = 0; k < array.Length; k++)
+        Console.Write(array[k] + " ");
+    Console.WriteLine();
+}
+
+Console.Write("Укажите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите минимальное значение массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите максимальное значение массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[] copyArr = CreateArr(size, min, max);
+ShowArray(copyArr);
+Console.WriteLine(searchNum(copyArr));
+
+
+// Экстра задача с семинара. Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+/*
+int[] CreateArr(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++) array[i] = new Random().Next(min, max);
+    
+    return array;
+}
+
+void showResult(int[] array)
+{
+    int total = 0;
+    for(int j = 0; j < Convert.ToInt32(array.Length / 2); j++)
+        Console.Write((total = array[j] * array[array.Length - 1 - j]) + " ");
+
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] copyArr = CreateArr(size, min, max);
+ShowArray(copyArr);
+showResult(copyArr);
+*/
---
+++ seminar6/Program.cs
@@ -0,0 +1,163 @@
+﻿// Задача семинар 1. 
+// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+/*
+bool TriangleCorrect(int side1, int side2, int side3)
+{
+
+    if(side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
+        return true;
+    else return false;
+}
+
+Console.Write("Введите размер массива: ");
+int side1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение массива: ");
+int side2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение массива: ");
+int side3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(TriangleCorrect(side1, side2, side3));
+*/
+
+// Задача семинар 2.
+// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+/*
+int[] Fibo()
+{
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int i = 2;
+    int[] array = new int[num];
+    array[0] = 0;
+    array[1] = 1;
+    while (i < num)
+    {
+        array[i] = array[i-1] + array[i-2];
+        i++;
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int k = 0; k < array.Length; k++)
+        Console.Write(array[k] + " ");
+    Console.WriteLine();
+}
+
+ShowArray(Fibo());
+Fibo();
+*/
+
+// Задача семинар 3. Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+/*
+int[] CopyArr(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+
+    for( int i = 0; i < array.Length; i++)
+        copyArray[i] = array[i];
+
+    return copyArray;
+}
+*/
+
+// Задача с ** семинар. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+/*
+string ConvertToByte(int n)
+{
+    string s = string.Empty;
+    while(n >= 2)
+    {
+        s = s + n % 2;
+        n = n / 2;
+    }
+    s = s + n;
+    return s;
+}
+
+
+string ReverseString(string text)
+{
+    string copyText = string.Empty;
+    int num = Convert.ToInt32(text.Length);
+    for(int i = 0; i < text.Length; i++)
+    {
+        char txt = text[num - i -1];
+        copyText += txt;
+    }
+
+    return copyText;
+}
+
+Console.Write("Введите число десятичной системы исчисления, которое нужно перевести в двоичную: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ReverseString(ConvertToByte(n)));
+*/
+
+// Задача 1 ДЗ. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+int SearchPositivNum(int num)
+{
+    int positivNum = 0;
+    for(int i = 1; i < num; i++)
+    {
+        Console.Write($"Введите {i} число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        if (num2 > 0)
+            positivNum++;
+    
+    }
+    return positivNum;
+}
+
+Console.Write("Введите количество чисел ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SearchPositivNum(num));
+*/
+
+// Задача 2 ДЗ. Напишите программу, которая найдёт точку пересечения двух прямых.
+/*
+void CrossLines(double k1, double b1, double k2, double b2)
+{
+    double x = Math.Round(((b2 - b1) / (k1 - k2)), 2);
+    double y = Math.Round((((k1 * (b2-b1)) / (k1 - k2)) + b1), 2);
+
+    if(k1 == k2)
+        Console.Write("Прямые являются параллельными (или совпадают), точки пересечения прямых нет.");
+    else
+        Console.Write($"Координаты точки пересечения заданных прямых ({x}; {y})");
+}
+
+Console.WriteLine("Чтобы увидеть координаты точки пересечения прямых y = k1x + b1, y = k2x + b2 введите следующие данные: ");
+Console.Write("Введите k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+CrossLines(k1, b1, k2, b2);
+*/
+
+
+string ConvertToByte(int n)
+{
+    string s = string.Empty;
+    while (n >= 2)
+    {
+        s = n % 2 + s;
+        n = n / 2;
+    }
+    s = n + s;
+    return s;
+}
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(ConvertToByte(n));
